928bd4474bd498357d39ca3647c2e0646b5629c3
Erwan Favry <efavry@users.noreply.github.com>
2015-10-29 16:16:21 +0100

    Correcting algorithm of longest path in a dag

--- longestPathInDag.cpp
+++ longestPathInDag.cpp
@@ -1,79 +1,109 @@
-void longestPath() //Beware this is pseudocode
+void graph::longestPath() //BEWARE MUST BE DONE ON A ACYCLIC DI-GRAPH
 {
-    this->unmarkAll(); //unmarkAll the node
-    list<node *> l_sorted; //will store the sorted node
-    bool unmarked_node_exist=true; 
-    node* elected_node=this->first; //when we find an unmarked node the pointer will be stored here for the next iteration of the while to be used for the call of traverse
-    while(unmarked_node_exist)
-    {
-        if(this->traverse(elected_node,l_sorted)==false)
-            return; //the graph is not a dag else the traverse suceeded and we need to elect a new node or get out of the loop
-        foreach(node *n:this->node_in_the _graph) //election d'un node 
-          if(!n->isMarked())
-            elected_node=n;
-    }
+  this->unmarkAll();
+  //because we have a dag we can do a topological sort and search with it
+  //tarjan made an easy to implement algorithm to do the topological sort
+  list<node *> l_sorted; //will store the sorted node
+  bool unmarked_node_exist=true;
+  node* elected_node=this->first; //when we find an unmarked node the pointer will be stored here for the next iteration of the while to be used for the call of traverse
+  cout << "Topological Sort : Starting \n";
+  while(unmarked_node_exist) //ugly incomming !
+  {
     
-    foreach (node *p:l_sorted)
-      foreach (node *o:p->succesors)
-            if (o->dist <= p->dist +1) //the weight of our edge is always 1 so +1 but 1 can bre replaced by weight of edges
-            {
-                o->dist=p->dist+1;
-                o->predecessor=p;
-            }
-            
-    //we traverse the graph to find the higest distance
-    node *end_of_longestpath;
+    if(this->topologicalSortRec(elected_node,l_sorted)==false)
+    {
+      cout << "Topological Sort : ERROR : NOT A DAG ! ABORTING \n";
+      return;
+    }
+    unmarked_node_exist=false;
+    for(node *n:this->listOfNodes) //told ya
+      if(!n->isMarkedNode())
+      {
+	unmarked_node_exist=true;
+	elected_node=n;
+      }
+  }
+  
+  this->printTopologicalSort(l_sorted);
+  
+  //for each node p of the sorted list we search wich node is the farthest from p
+  for (node *p:l_sorted)
+    for (node *o:p->l_successors)
+      if (o->int_dist <= p->int_dist +1) //the weight of our edge is always 1 so +1
+      {
+	o->int_dist=p->int_dist+1;
+	o->predecessor=p;
+      }
+      
+      //we traverse the graph to find the higest distance
+      node *end_of_longestpath=NULL;
     int max_dist=0;
-    foreach(node *v:this->node_in_the _graph)
-        if(v->dist>max_dist)
-        {
-            max_dist=v->dist;
-            end_of_longestpath=v;
-        }
-        
-    this->print(this->reconstruct(end_of_longestpath))
-    this->unmarkAll(); //we go back to a stable state for the graph
+  max_dist=this->findMaximumDistance(end_of_longestpath);
+  
+  //then from the end_of_longestpath we get each of his predecessors and we show the result while popping the stach (a list here, yes a list...)
+  this->printLongestPath(this->reconstructFromEnd(end_of_longestpath),max_dist);
+  this->unmarkAll();
 }
 
-stack graph::reconstruct(node *end_of_longestpath)
+int graph::findMaximumDistance(node *end_of_longestpath=NULL)
 {
-  //from the end_of_longestpath we get each of his predecessors:
-  stack path;
+  int max_dist=0;
+  for(node *v:this->listOfNodes)
+    if(v->int_dist>max_dist)
+    {
+      max_dist=v->int_dist;
+      end_of_longestpath=v;
+    }
+    return max_dist;
+}
+
+//return false if not a dag
+bool graph::topologicalSortRec(node *n, list<node *> &l_sorted)
+{
+  if(n->b_tempMark) //flemme to do accessors
+    return false; //if it is marked temporarly then it was also a n in a upper call of traverse, there fore we hav a cycle
+    
+    if(!n->isMarkedNode())
+    {
+      n->b_tempMark=true;
+      for(node *m:n->l_successors)
+	this->topologicalSortRec(m,l_sorted); //for each child of n we call recursivly
+	n->markNode(); //we mark the node n because all operation are finished with him
+	n->b_tempMark=false; //
+	l_sorted.push_front(n); //we add n to the list of sorted node
+    }
+    return true;
+}
+
+void graph::printTopologicalSort(list<node *> &l_sorted)
+{
+  cout << "Topological Sort : Sorted list is : ";
+  for(node *m:l_sorted)
+    cout << m->uint_name << ";";
+  cout << "\n";
+}
+
+list<unsigned int> graph::reconstructFromEnd(node *end_of_longestpath)
+{
+  list<unsigned int> path;
   while(end_of_longestpath->predecessor != NULL)
   {
-    path.push(end_of_longestpath);
+    path.push_back(end_of_longestpath->uint_name);
     end_of_longestpath=end_of_longestpath->predecessor;
-    for(node* to_test:path)
-      if(to_test == end_of_longestpath)
+    for(unsigned int to_test:path)
+      if(to_test == end_of_longestpath->uint_name)
 	end_of_longestpath->predecessor=NULL;
   }
+  return path;
 }
 
-void graph::print(stack path)
+void graph::printLongestPath(list<unsigned int> path, int max_dist)
 {
-  //then we show the result while popping the stack
-  printline("A longest path is found of size " << max_dist << ": ");
+  cout<< "A longest path is found of size "<< max_dist << ": " << endl;
   while(!path.empty())
   {
-    print(path.back() << ";";
+    cout<< path.back() << ";";
     path.pop_back();
   }
   cout<< endl;
-}
-
-bool graph::traverse(node *n, list<node *> &l_sorted)
-{
-    if(n->marked)
-        return false; //if it is marked temporarly then it was also a n in a upper call of traverse, there fore we hav a cycle
-
-    if(!n->isMarkedN))
-    {
-        n->marked=true;
-        foreach(node *m:n->succesors)
-            this->traverse(m,l_sorted); //for each child of n we call recursivly
-        n->markNode(); //we mark the node n because all operation are finished with him
-        n->marked=false; //
-        l_sorted.push_front(n); //we add n to the list of sorted node
-    }
-    return true;
 }
